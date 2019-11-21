80591b693a7e589255aeb27154b19a13733289b1
Leonid8000 <leonidzp8000@ukr.net>
2019-11-21 17:56:51 +0200

    tag controller

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
